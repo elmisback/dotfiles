c49e62fc74e97e1e17512606b5842465d035f9a4
ulmusic <elmisback@gmail.com>
2015-11-17 04:28:59 -0500

    Trying GNU stow on for size

--- install.zsh
+++ install.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# Dependencies: zsh, curl, git.
+# Dependencies: zsh, curl, git, and GNU stow.
 
 # TODO
 # * Work out bootstrapping process.
@@ -17,7 +17,7 @@ source $dotfiles/zsh/.zshenv
 echo "Generating internal configuration symlinks..."
 
 printf "vim... "
-mkdir $dotfiles/nvim/autoload 2>/dev/null
+mkdir $dotfiles/nvim/autoload 2>/dev/null || :
 ln -sf $dotfiles/nvim/init.vim $dotfiles/vim/.vimrc
 ln -sf $dotfiles/nvim/autoload $dotfiles/vim/autoload
 echo "[x]"
@@ -30,15 +30,25 @@ echo "[x]"
 echo ""
 
 echo "Linking $dotfiles to $XDG_CONFIG_HOME..."
-if [ -d $XDG_CONFIG_HOME ]; then
-  read -q "REPLY?$XDG_CONFIG_HOME will be overwritten. Back up to ${XDG_CONFIG_HOME}_old?[y/n]"
-  if [ $REPLY = 'y' ]; then
-    echo "Backing up..."
-    cp -r $XDG_CONFIG_HOME ${XDG_CONFIG_HOME}_old
-  fi
-fi
-ln -sF $dotfiles $XDG_CONFIG_HOME
 
+# Make sure $XDG_CONFIG_HOME exists first.
+mkdir $XDG_CONFIG_HOME 2>/dev/null || :
+for pkg in $(ls -d $dotfiles/*); do
+  mkdir $XDG_CONFIG_HOME/$pkg 2>/dev/null || :
+  stow --dir=$dotfiles --target=$XDG_CONFIG_HOME/$pkg $pkg
+done
+
+# function manual_link {
+#   if [ -d $XDG_CONFIG_HOME ]; then
+#     stow
+#     read -q "REPLY?$XDG_CONFIG_HOME will be overwritten. Back up to ${XDG_CONFIG_HOME}_old?[y/n]"
+#     if [ $REPLY = 'y' ]; then
+#       echo "Backing up..."
+#       cp -r $XDG_CONFIG_HOME ${XDG_CONFIG_HOME}_old
+#     fi
+#   fi
+#   ln -sF $dotfiles $XDG_CONFIG_HOME
+# }
 
 #if [ ! -e $antigen ]; then
 #  git clone $antigen_url $antigen
