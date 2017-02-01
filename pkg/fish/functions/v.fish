230288b996cc9d477931f8896fc4f38c93a7f1b9
Edward Misback <elmisback@gmail.com>
2017-02-01 16:23:12 -0500

    Start using fzf + fasd with v.

--- pkg/fish/functions/v.fish
+++ pkg/fish/functions/v.fish
@@ -1,7 +1,8 @@
 function v --description 'Quickly open files with vim'
+  # TODO: if isdir, use fzf on the dir
   if test -z (fasd -f "$argv")
-    vim "$arg"
+    vim "$argv"
   else
-    fasd -fe vim "$argv"
+    fasd -Rfl "$argv" | fzf -1 -0 --no-sort -m | tr '\n' ' '| read -l -a result; and vim $result
   end
 end
